4a424d132f0315e64a76b98485b31aa7c6619710
Nathaniel Furniss <nlfurniss@gmail.com>
2018-04-13 11:00:59 -0700

    Update ember-cli

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,10 @@
-import resolver from './helpers/resolver';
-import { setResolver } from 'ember-qunit';
-
+import Application from '../app';
+import config from '../config/environment';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
 import registerPolyfills from './helpers/polyfills/register';
-registerPolyfills();
 
-setResolver(resolver);
+registerPolyfills();
+setApplication(Application.create(config.APP));
+
+start();
